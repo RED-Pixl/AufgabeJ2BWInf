8a613ec7dda73a3ef3a009f9b48349898cef2f7e
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2023-11-03 16:54:01 +0100

    Solved the task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        //args = new String[]{"src/main/resources/bild01.png"};
+
+        if (args.length == 0) {
+            throw new IllegalArgumentException("file to read from is missing");
+        }
+
+        final BufferedImage image = ImageIO.read(new File(args[0]));
+
+        final StringBuilder sb = new StringBuilder();
+
+        int x = 0;
+        int y = 0;
+        Color color;
+
+        do {
+            color = new Color(image.getRGB(x, y));
+            sb.append((char) color.getRed());
+            x += color.getGreen();
+            y += color.getBlue();
+            x %= image.getWidth();
+            y %= image.getHeight();
+        } while (color.getGreen() != 0 && color.getBlue() != 0);
+
+        System.out.println();
+        System.out.println(sb);
     }
 }
